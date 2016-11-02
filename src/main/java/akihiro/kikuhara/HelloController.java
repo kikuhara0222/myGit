163b2737384e844639aa3a87cefa14a52c284d5e
菊原 聡宏 <a.kikuhara@alpha.local>
2016-11-02 16:28:20 +0900

    th:eachのテスト

--- src/main/java/akihiro/kikuhara/HelloController.java
+++ src/main/java/akihiro/kikuhara/HelloController.java
@@ -1,18 +1,44 @@
 package akihiro.kikuhara;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Model model) {
-		model.addAttribute("msg", "Springloadedを導入");
+		model.addAttribute("msg", "ようこそ。");
 		return "index";
 	}
+
+	@RequestMapping(value = "/", method=RequestMethod.POST)
+	public String send(Model model) {
+		
+		TestData color1 = new TestData();
+		TestData color2 = new TestData();
+		TestData color3 = new TestData();
+		
+		color1.setColorName("あか");
+		color1.setColorKanji("赤");
+		color2.setColorName("みどり");
+		color2.setColorKanji("緑");
+		color3.setColorName("あお");
+		color3.setColorKanji("青");
+		
+		ArrayList<TestData> dataAry = new ArrayList<TestData>();
+		dataAry.add(color1);
+		dataAry.add(color2);
+		dataAry.add(color3);
+		
+		model.addAttribute("msg", "三原色");
+		model.addAttribute("colors", dataAry);
+		
+		return "index";
+	}
+
 }
---
+++ src/main/java/akihiro/kikuhara/TestData.java
@@ -0,0 +1,19 @@
+package akihiro.kikuhara;
+
+public class TestData {
+	public String colorName;
+	public String colorKanji;
+	
+	public String getColorName() {
+		return colorName;
+	}
+	public void setColorName(String colorName) {
+		this.colorName = colorName;
+	}
+	public String getColorKanji() {
+		return colorKanji;
+	}
+	public void setColorKanji(String colorKanji) {
+		this.colorKanji = colorKanji;
+	}
+}
