7182346fe40c0a664ff23b3773b9ecbb63841a45
菊原 聡宏 <a.kikuhara@alpha.local>
2016-11-02 18:32:44 +0900

    比較演算子など追加

--- src/main/java/akihiro/kikuhara/HelloController.java
+++ src/main/java/akihiro/kikuhara/HelloController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,29 +18,46 @@ public class HelloController {
 		return "index";
 	}
 
-	@RequestMapping(value = "/", method=RequestMethod.POST)
+	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public String send(Model model) {
-		
+
 		TestData color1 = new TestData();
 		TestData color2 = new TestData();
 		TestData color3 = new TestData();
-		
+
 		color1.setColorName("あか");
 		color1.setColorKanji("赤");
 		color2.setColorName("みどり");
 		color2.setColorKanji("緑");
 		color3.setColorName("あお");
 		color3.setColorKanji("青");
-		
+
 		ArrayList<TestData> dataAry = new ArrayList<TestData>();
 		dataAry.add(color1);
 		dataAry.add(color2);
 		dataAry.add(color3);
-		
+
 		model.addAttribute("msg", "三原色");
 		model.addAttribute("colors", dataAry);
-		
+
 		return "index";
+	}
+
+	@RequestMapping(value = "/page1")
+	public String page1(Model model) {
+		model.addAttribute("name", "てすと");
+		return "page1";
+	}
+	
+	@RequestMapping(value="/page2",method=RequestMethod.GET)
+	public String page2(Model model){
+		return "page2";
+	}
+	
+	@RequestMapping(value="/page2",method=RequestMethod.POST)
+	public String page2(@ModelAttribute Page2Form form,Model model){
+		model.addAttribute("form", form);	
+		return "page2";
 	}
 
 }
