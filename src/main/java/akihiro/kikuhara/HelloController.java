bfdf238116783cf06d59bb8a4218214b143b995a
菊原 聡宏 <a.kikuhara@alpha.local>
2016-11-02 15:53:57 +0900

    Springloadedを導入

--- src/main/java/akihiro/kikuhara/HelloController.java
+++ src/main/java/akihiro/kikuhara/HelloController.java
@@ -10,28 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/")
 	public String index(Model model) {
-		model.addAttribute("msg", "testMessage");
-		return "index";
-	}
-
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public String send(@RequestParam(value="check1",required=false)boolean check1,
-			@RequestParam(value="check2",required=false)boolean check2,
-			@RequestParam(value="select1",required=false)String radio1,Model model) {
-		
-		String retMsg = "";
-		
-		try{
-			retMsg = "check1:" + check1 + " check2:"+check2 +
-					" radio:" + radio1;
-		}catch (NullPointerException e){
-			retMsg = "入力無";
-		}
-		
-		model.addAttribute("msg",retMsg);
-		
+		model.addAttribute("msg", "Springloadedを導入");
 		return "index";
 	}
 }
